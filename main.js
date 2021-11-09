ecf02e07971ea200c7f81a0ac588323f502d0560
Melanie Arellano <melaniesigrid@protonmail.com>
2021-11-09 14:53:10 -0600

    Correct linter errors

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.mainMenu');
 const openMenu = document.querySelector('.openMenu');
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
-
 function show() {
   mainMenu.style.display = 'flex';
   mainMenu.style.top = '0';
 }
+
 function close() {
   mainMenu.style.top = '-100%';
 }
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
