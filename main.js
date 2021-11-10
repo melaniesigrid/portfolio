cafbec44d6d610792ef19601bc453d564e0c5d70
Melanie Arellano <melaniesigrid@protonmail.com>
2021-11-10 15:57:32 -0600

    Fix linter error

--- main.js
+++ main.js
@@ -14,39 +14,39 @@ function close() {
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
-openModalButtons.forEach(button => {
+openModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
   })
 })
 
 overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
+  const modals = document.querySelectorAll('.modal.active');
   modals.forEach(modal => {
-    closeModal(modal)
+    closeModal(modal);
   })
 })
 
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
+    const modal = button.closest('.modal');
+    closeModal(modal);
   })
 })
 
 function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
 }
 
 function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
 }
