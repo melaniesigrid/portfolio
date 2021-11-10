699cca2737eef1c05a78804bec4c32500b9e2af9
Melanie Arellano <melaniesigrid@protonmail.com>
2021-11-10 15:44:56 -0600

    Add first modal prototype with html, css and js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Hamburger menu open-close
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.mainMenu');
 const openMenu = document.querySelector('.openMenu');
@@ -13,3 +14,41 @@ function close() {
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
+
+//See-Project modals
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
