3b580ef71f4fc73d66b4338127386333e3d9d15b
Melanie Arellano <melaniesigrid@protonmail.com>
2021-11-11 09:19:01 -0600

    Fix linter errors

--- main.js
+++ main.js
@@ -34,19 +34,19 @@ openModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
-  })
-})
+  });
+});
 
 overlay.addEventListener('click', () => {
   const modals = document.querySelectorAll('.modal.active');
   modals.forEach(modal => {
     closeModal(modal);
   })
-})
+});
 
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = button.closest('.modal');
     closeModal(modal);
-  })
-})
+  });
+});
